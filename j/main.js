58bb29e8151ea23d7465518eec3c05abb0a2dcd2
parisminton <parisminton@da.ydrea.ms>
2011-05-23 08:25:46 -0400

    Added a directory for scripts and a main.js file; added several images to the art folder; broke the CSS into separate files and added rollovers to the home page.

---
+++ j/main.js
@@ -0,0 +1,181 @@
+(function () {
+
+  this.bW = {
+  
+    // event handler helpers
+    evts : {
+    
+      listenFor : function (node, evt, func, capt, aargs) {
+        var proc_id;
+        if (node.addEventListener) {
+          node.addEventListener(evt, func, capt);
+        }
+        else {
+          evt = "on" + evt;
+          if (node.attachEvent) {
+            node.attachEvent(evt, func);
+          }
+          else {
+            node[evt] = func;
+          }
+        }
+        proc_id = ("process" + (this.registry.count + 1));
+        this.registry[proc_id] = {};
+        this.registry[proc_id].node = node;
+        this.registry[proc_id].evt = evt;
+        this.registry[proc_id].func = func;
+        this.registry[proc_id].capt = capt;
+        this.registry[proc_id].aargs = aargs;
+        
+        if (!func.reg_ids) {
+          func.reg_ids = [];
+        }
+        func.reg_ids.push(proc_id);
+        
+        this.registry.count += 1;
+      },
+      
+      stopListening : function (node, evt, func, capt) {
+        if (node.removeEventListener) {
+          node.removeEventListener(evt, func, capt);
+        }
+        else {
+          evt = 'on' + evt;
+          if (node.detachEvent) {
+            node.detachEvent(evt, func);
+          }
+          else {
+            node[evt] = null;
+          }
+        }
+      },
+      
+      registry : { count : 0 },
+      
+      identify : function (evt) {
+        evt = evt || window.event;
+        evt.src = evt.target || evt.srcElement;
+        return evt;
+      }
+    },
+    
+    imgswaps : {
+      
+      preloadTogglers : function (path) {
+        var images = [],
+            args = [],
+            i = 0,
+            len = arguments.length,
+            shortname;
+        if (len == 2 && typeof arguments[1] == "object") {
+          len = arguments[1].length;
+          for (i; i < len; i += 1) {
+            args[i] = arguments[1][i];
+          }
+        }
+        else {
+          for (i; i < (len - 1); i += 1) {
+            args[i] = arguments[(i + 1)];
+          }
+        }
+        i = 0;
+        len = args.length;
+        for (i; i < len; i += 1) {
+          shortname = bW.strings.getFileName(bW.styles.getStyle(args[i], "background-image", "backgroundImage"));
+          images[i] = new Image();
+          images[i].src = path + shortname;
+          if (shortname.match(/_off\.|_on\./)) {
+            images[(i + len)] = new Image();
+            images[(i + len)].src = bW.strings.toggler(shortname);
+          }
+        }
+      },
+      
+      toggleBg : function (evt) {
+        var ev = bW.evts.identify(evt),        
+            image_path = bW.styles.getStyle(ev.src.parentNode, "background-image", "backgroundImage").split(/_off\.|_on\./),
+            suffix;
+        
+        if (ev.src.nodeName == "A") {
+          if (ev.type == "mouseover") {
+            suffix = "_on.";
+          }
+          else {
+            suffix = "_off.";
+          }
+          
+          ev.src.parentNode.style.backgroundImage = image_path[0] + suffix + image_path[1];
+    
+        }
+      }
+    },
+    
+    // style retreival
+    styles : {
+      getStyle : function (elem, dom_prop, ie_prop) {
+        if (window.getComputedStyle) {
+          return window.getComputedStyle(elem, null).getPropertyValue(dom_prop);
+        }
+        else if (elem.currentStyle) {
+          return elem.currentStyle[ie_prop];
+        }
+      }
+    },
+    
+    // string manipulation
+    strings : {
+      getFileName : function (string) {
+        var filename,
+            index1 = (string.lastIndexOf("/") + 1),
+            index2,
+            char2 = "";
+        if (string.lastIndexOf("\"") != -1) {
+          char2 = "\"";
+          index2 = string.lastIndexOf(char2);
+          filename = string.substring(index1, index2);
+        }   
+        else {
+          filename = string.substring(index1);
+        }   
+        return filename;
+      },
+      
+      toggler : function (filename) {
+        var find = /_off\.|_on\./,
+            match = filename.match(find),
+            suffix,
+            new_filename,
+            split_array;
+        if (match == "_off.") {
+          suffix = "_on.";
+        }
+        else {
+          suffix = "_off.";
+        }
+        split_array = filename.split(match);
+        new_filename = split_array[0] + suffix + split_array[1];
+        return new_filename;
+      }
+    },
+    
+    // document references
+    page : {
+      the_nav : document.getElementById("nav"),
+      nav_lis : [
+        document.getElementById("nav-about"),
+        document.getElementById("nav-work"),
+        document.getElementById("nav-contact")
+      ]
+    }
+  };
+
+}());
+
+bW.imgswaps.preloadTogglers("a/", bW.page.nav_lis);
+
+bW.evts.listenFor(bW.page.the_nav, "mouseover", bW.imgswaps.toggleBg, false, bW.page.nav_lis);
+
+bW.evts.listenFor(bW.page.the_nav, "mouseout", bW.imgswaps.toggleBg, false, bW.page.nav_lis);
+  
+alert("I can take you there. Just follow me.");
+
