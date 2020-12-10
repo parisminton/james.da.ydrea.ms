f025aa34535f3df58c3670812da1166f7756b775
James Thomas <parisminton@da.ydrea.ms>
2020-12-10 15:24:01 -0500

    Update index copy.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,9 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1 className="nameplate">James Thomas</h1>
-    <p className="body-copy">For two decades I've worked for the digital platforms of large news organizations. Since 2012, I've been a software engineer for the Interactive News department of The New York Times. Before that, I spent 11 years as Web Editor for the Detroit Free Press.</p>
+    <p className="body-copy">For more than two decades I've worked for the digital platforms of large news organizations. Since 2012, I've been a software engineer in the Interactive News department at The New York Times. Before that, I spent 11 years as Web Editor for the Detroit Free Press.</p>
+    <p className="body-copy">Along the way I've written stories, shot photos and video, made illustrations, designed pages, taught reporters to use technology and helped them brainstorm digital ideas. I've put code on the Times and Free Press home pages and ink in their print editions.</p>
+    <p className="body-copy">I've immensely enjoyed my career and don't know how I got so lucky. But I'll admit the sweet spot still eludes me. I'm looking for more ways to use a range of narrative tools cohesively in a project's development. I seek more opportunities to work on stories across their life cycles: conception, reporting and presentation.</p>
   </Layout>
 )
 
